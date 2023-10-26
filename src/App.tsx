bd32f44e570aeb9f341e3c580012f76ee97f999a
Felipe Soares <felipe.11.11@hotmail.com>
2023-10-26 14:13:15 -0400

    Feat: Create first route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { DefaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
