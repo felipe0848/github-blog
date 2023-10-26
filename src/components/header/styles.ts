144a5af9035b061c86169759ca8274cd151425ed
Felipe Soares <felipe.11.11@hotmail.com>
2023-10-26 14:12:43 -0400

    Feat: Create Header and Home component

---
+++ src/components/header/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  margin-top: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+export const BackgroundImg = styled.img`
+  position: absolute;
+  width: 100vw;
+  inset: 0;
+  z-index: -999;
+`
